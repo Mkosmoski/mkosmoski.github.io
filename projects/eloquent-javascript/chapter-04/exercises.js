0b2484192330e8599f8244d54f15e44f5ecf26a2
Ubuntu <ubuntu@ip-172-31-33-87.ec2.internal>
2020-08-24 19:12:20 +0000

    eloquent javascript and recursion

--- projects/eloquent-javascript/chapter-04/exercises.js
+++ projects/eloquent-javascript/chapter-04/exercises.js
@@ -61,33 +61,54 @@ function reverseArray(array) {
 
 function reverseArrayInPlace(array) {
   
-  array.forEach(function(num, index, array) {
-  range(array[0], array[array.length-1])
-
-})
+  for (let i = 0; i < Math.floor(array.length / 2); i++) {
+    let swap = array[i];
+    array[i] = array[array.length-i-1];
+    array[array.length-i-1] = swap;
+  }
+  return array;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // arrayToList /////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function arrayToList() {
-
+function arrayToList(array) {
+  let arrayList = null;
+  
+  for(let i = array.length-1; i >= 0; i--){
+    arrayList = { value: array[i], rest: arrayList }
+  }
+return arrayList
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // listToArray /////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
-
-function listToArray() {
-
+//"converts list into array"
+//param list
+function listToArray(list) {
+ let result = [];
+  if (typeof list === 'undefined' || list.value === undefined || list.rest === undefined) {
+    return result;
+  } else {
+    result.push(list.value);
+    while (list.hasOwnProperty('rest') && list.rest !== null) {
+      list = list.rest;
+      if (list.hasOwnProperty('value')) {
+      	result.push(list.value);
+      }
+    }
+  }
+  return result;
 }
-
 ////////////////////////////////////////////////////////////////////////////////
 // prepend /////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
-
-function prepend() {
+//takes a number and adds it to the front of an arraytolist array
+function prepend(num, list) {
+  return {value: num, rest: list}
+  
 
 }
 
@@ -95,17 +116,56 @@ function prepend() {
 // nth /////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function nth() {
-
+function nth(list, number) {
+  if (number === 0) {
+    return list.value;
+  } else if (list.rest === null) {
+    return undefined;
+  } else {
+    return nth(list.rest, number-1);
+  }
 }
+
 
 ////////////////////////////////////////////////////////////////////////////////
 // deepEqual ///////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function deepEqual() {
-
+function deepEqual(thing1, thing2) {
+  
+  if(!thing1 && !thing2){
+    return false;
+  } else if(typeof thing1 !== typeof thing2){
+    return false;
+  }else if (typeof thing1 === 'number'){
+    return thing1 === thing2
+  
+    
+  } else if (typeof thing1 === 'object') {
+  
+      for(let keys in thing1){
+         if(!(keys in thing2) || !deepEqual(thing1[keys], thing2[keys]))
+         return false;
+      }
+      return true;
+    
+  } else{
+    return thing1 === thing2;
+  }
 }
+
+
+
+/*
+  for (let prop in a) {
+      if (!(prop in b) || !deepEqual(a[prop], b[prop])) return false;
+    }
+    return true;
+  } else {
+    return a === b;
+  }
+}*/
+
 
 ////////////////////////////////////////////////////////////////////////////////
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
--- projects/eloquent-javascript/chapter-05/exercises.js
+++ projects/eloquent-javascript/chapter-05/exercises.js
@@ -1,34 +1,113 @@
 // /////////////////////////////////////////////////////////////////////////////
 // flatten /////////////////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
+//flattens two dimensional array into single array
+//reduce method in combination with the concat method to “flatten” 
+//an array of arrays into a single array
 
-function flatten() {
+//param array of arrays
+function flatten(arr) {
+  
+ return arr.reduce((combine, curr) => {
+   return combine.concat(curr)
+   
+  }, )
+  
 
 }
 
 // /////////////////////////////////////////////////////////////////////////////
 // loop ////////////////////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
+//It takes a value, a test function, an update function, and a body function. 
+//Each iteration, it first runs the test function on the current loop value and 
+//stops if that returns false. Then it calls the body function, giving it the current value. 
+//Finally, it calls the update function to create a new value and starts from the beginning.
 
-function loop() {
-
+function loop(value, test, update, func) {
+      //test if true or false, stop if false
+      if(test(value)){
+        func(value);
+      } else {
+        return false
+     }
+     return loop(update(value), test, update, func)
+        
 }
 
 // /////////////////////////////////////////////////////////////////////////////
 // every ///////////////////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
+//should determine if every value passes a test
+//takes in an array and a test
 
-function every() {
-
+function every(array, test) {
+  if(!array.length){
+    return true;
+  }
+ let result = false;
+  array.forEach(elem => test(elem) ? result = true : result = false )
+return result
 }
 
 // /////////////////////////////////////////////////////////////////////////////
 // dominantDirection ///////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
 
-function dominantDirection() {
-
+//given by the book
+function characterScript(code) {
+  for (let script of SCRIPTS) {
+    if (script.ranges.some(([from, to]) => code >= from &&
+                                           code < to)) {
+      return script;
+    }
+  }
+  return null;
 }
+
+function countBy(items, groupName) {
+  let counts = [];
+  for (let item of items) {
+    let name = groupName(item);
+    let known = counts.findIndex(c => c.name == name);
+    if (known == -1) {
+      counts.push({name, count: 1});
+    } else {
+      counts[known].count++;
+    }
+  }
+  return counts;
+}
+
+
+
+///////////////////////
+
+
+//computes the dominant writing direction in a string of text
+//param is text 
+function dominantDirection(text) {
+    let scripts = countBy(text, char => {
+    let script = characterScript(char.codePointAt(0));
+    return script ? script.direction : "none";
+  }).filter(({name}) => name != "none");
+  if(!scripts.length){
+    return 'no dominant direction found';
+  }else if(scripts.length === 1){
+     return scripts[0].name;
+  } else {
+    
+    if (scripts.reduce((acc, cur) => acc.count === cur.count)) {
+        return 'no dominant direction found';
+      } else {
+        return scripts.reduce((acc, cur) => acc.count >= cur.count ? acc.name : cur.name);
+      }
+  }
+}
+  
+  
+
+
 
 // /////////////////////////////////////////////////////////////////////////////
 //  //////////////////////////////////////////////////////
